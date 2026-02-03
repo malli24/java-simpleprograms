b81ea941193a981429019e208e38721b98d81cce
malli24 <malleswari82006@gmail.com>
2026-02-03 14:42:56 +0530

    Add files via upload

---
+++ Starttoendsame.java
@@ -0,0 +1,11 @@
+public class Starttoendsame {
+    public static void main(String[] args) {
+        String str="level";
+        if(str.charAt(0)==str.charAt(str.length()-1)){
+            System.out.println("Starts and ends with same charcter");
+        }
+        else{
+            System.out.println("different start and end");
+        }
+    }
+}
